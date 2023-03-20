7c3cddd9e8f30e2c8447f3dae8ef0d9040cd57eb
rafaelsolanogit config --global user.name rafaelsolanogit config --global user.name rafaelsolano <rafaelsolanol007@gmail.com>
2023-03-20 01:45:12 -0500

    finaliza clase 5 y curso Diom

--- script.js
+++ script.js
@@ -1,57 +1,36 @@
-const btn = document.querySelector('[data-form-btn]');
+import checkComplete from "./components/checkComplete.js";
+import deletIcon from "./components/deleteIcon.js";
 
-console.log(btn);
+  const btn = document.querySelector('[data-form-btn]');
 
+  //Arrow function
 
-//Arrow function
+  const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value;
+    const list =document.querySelector('[data-list]')
+    const task = document.createElement('li');
+    task.classList.add("card")
+    input.value="";
+    
 
-const createTask = (evento) => {
-  evento.preventDefault();
-  const input = document.querySelector('[data-form-input]');
-  const value = input.value;
-  const list =document.querySelector('[data-list]')
-  const task = document.createElement('li');
-  task.classList.add("card")
-  input.value="";
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText =value;
+    taskContent.appendChild(titleTask);
+
+    
+    task.appendChild(taskContent);
+    task.appendChild(deletIcon());
+    list.appendChild(task);
+ 
+
+  };
   
-
-  const taskContent = document.createElement("div");
-  taskContent.appendChild(checkComplete());
-  const titleTask = document.createElement("span");
-  titleTask.classList.add("task");
-  titleTask.innerText =value;
-  taskContent.appendChild(titleTask);
+  //listener
+  btn.addEventListener('click',  createTask);
 
 
-  
-  const content =`
-  
-<i class="fas fa-trash-alt trashIcon icon"></i>`;
-
-  
-task.appendChild(taskContent);
-list.appendChild(task);
-console.log(content);
-  
-};
-console.log(btn);
-
-//listener
-btn.addEventListener('click',  createTask);
-
-const checkComplete = () =>{
-  const i = document.createElement("i");
-  i.classList.add("far", "fa-check-square", "icon" );
-  i.addEventListener('click',  completeTask)
-  return i;
-}
-
-
-const completeTask = (event)=>{
-  
-  const element = event.target
-  element.classList.toggle('fas' )
-  element.classList.toggle('far')
-  element.classList.toggle('completeIcon')
-  
-}
